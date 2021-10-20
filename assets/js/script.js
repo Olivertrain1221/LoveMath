e75878d5105e48a89839cf15f67ab72e53996076
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-20 11:43:51 +0000

    Add base js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-  function runGame(){
+ //wait for the DOM to finish loading before running the game
+ // Get the button elements and add event listeners to them
+ 
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+ function runGame(){
 
   }
 
@@ -6,26 +25,26 @@
 
   }
 
-  function calculateCorrectAnswer(){
+  function calculateCorrectAnswer() {
 
   }
 
-  function incrementScore(){
+  function incrementScore() {
 
   }
 
-  function incrementWrongAnswer(){
+  function incrementWrongAnswer() {
 
   }
 
-  function displayAdditionQuestiopn (){
+  function displayAdditionQuestiopn () {
 
   }
 
-  function displaySubtractQuestion(){
+  function displaySubtractQuestion() {
 
   }
 
-  function displayMultiplyQuestion (){
-      
+  function displayMultiplyQuestion () {
+
   }
