ce3b72beb81634be827bbbd7c517e21293bd071c
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-20 11:21:24 +0000

    Set up structure of game functions in js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+  function runGame(){
+
+  }
+
+  function checkAnswer() {
+
+  }
+
+  function calculateCorrectAnswer(){
+
+  }
+
+  function incrementScore(){
+
+  }
+
+  function incrementWrongAnswer(){
+
+  }
+
+  function displayAdditionQuestiopn (){
+
+  }
+
+  function displaySubtractQuestion(){
+
+  }
+
+  function displayMultiplyQuestion (){
+      
+  }
