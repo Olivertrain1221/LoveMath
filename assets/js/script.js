52ea98756bbd2bb0fb94ade3fa77e92b14f81436
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-22 13:29:39 +0000

    Add division functionality to the js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,13 +39,17 @@ function runGame(gameType){
     //Creates two random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let div2 = Math.floor(Math.random() * 25) + 1;
+    let div1 = div2 * (Math.floor(Math.random() * 10) + 1);
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract" ) {
+    } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType=== "divide") {
+        displayDivisionQuestion(div1, div2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting`;   
@@ -101,7 +105,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/"){
+        return [operand1 / operand2, "divide"];
+    }
+    else {
         alert (`Unimplemented operator ${operator}`);
         throw ` Unimplemented operator ${operator}. Aborting!`;
     }
@@ -143,3 +150,9 @@ function displayMultiplyQuestion (operand1, operand2) {
       document.getElementById('operand2').textContent = operand2;
       document.getElementById('operator').textContent = "x";
     }
+
+function displayDivisionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 
+    document.getElementById('operand2').textContent = operand2 
+    document.getElementById('operator').textContent = "/";
+}
